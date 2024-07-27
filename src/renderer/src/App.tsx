60a93eaf04fa1886f443050c704c1048e9fe4ee7
Deyweson <deywesonalm@gmail.com>
2024-07-27 19:04:52 -0300

    Initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,13 @@
+import { PlayByutton } from "./components/PlayButton";
+
+function App(): JSX.Element {
+
+  return (
+    <>
+      <PlayByutton />
+      <img src="./assets/play.png" alt="123" />
+    </>
+  )
+}
+
+export default App
